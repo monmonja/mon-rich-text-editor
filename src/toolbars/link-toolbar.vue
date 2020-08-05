1636785e887aaf1b3fa5241fdcd2b8500561d1fe
almond <almondmendoza@gmail.com>
2020-08-06 04:54:18 +1000

    add remove link

--- src/toolbars/link-toolbar.vue
+++ src/toolbars/link-toolbar.vue
@@ -6,6 +6,7 @@
             <input type="text" placeholder="html" v-model="linkTitle"/>
             <input type="text" placeholder="target" v-model="target"/>
             <button type="button" class="btn btn-small btn-inline" @click="addLink"  >Apply</button>
+            <button v-if="isLink" type="button" class="btn btn-small btn-inline" @click="removeLink"  >Remove Link</button>
             <button type="button" class="btn btn-small btn-inline" @click="setActiveToolbar('main')"  >Cancel</button>
         </li>
     </ul>
@@ -33,6 +34,7 @@
         private linkUrl: string = ''
         private linkTitle: string = ''
         private target: string = ''
+        private isLink: boolean = false
 
         public addLink () : void {
             let selectedHtml = this.getSelectedHtml(this.iframe);
@@ -47,6 +49,23 @@
             let html = `<a href="${this.linkUrl}" ${target}>${selectedHtml}</a>`
             this.iframe.contentWindow.document.execCommand('insertHTML', false, html);
             this.$root.$emit('mon-iframe-changed');
+
+            this.linkUrl = '';
+            this.linkTitle = '';
+            this.target = '';
+            this.isLink = false;
+            this.setActiveToolbar('main');
+        }
+
+        public removeLink () : void {
+            let windowIframe = this.iframe.contentWindow as Window;
+            let sel = windowIframe.getSelection();
+            let r = sel.getRangeAt(0);
+            let el = r.startContainer.parentElement;
+            el.parentNode.removeChild(el)
+
+            this.iframe.contentWindow.document.execCommand('insertHTML', false, el.innerHTML);
+            this.$root.$emit('mon-iframe-changed');
             this.setActiveToolbar('main');
         }
 
@@ -58,6 +77,8 @@
                         if (currentNode.nodeName.toLowerCase() === 'a') {
                             this.linkTitle = currentNode.innerHTML;
                             this.linkUrl = currentNode.getAttribute('href');
+                            this.target = currentNode.getAttribute('target');
+                            this.isLink = this.linkUrl !== null;
                         }
                     }
                 }
